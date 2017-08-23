a9d7f2448eb728d33b302512a8d91033fe6a78da
Aleksandar Karakas <aleksandar.karakas@student.tugraz.at>
2017-08-23 13:02:16 +0200

    put version number in one single place
    Now, when the version changes, it needs to be changed only in setup.py (and not additionally in doc/conf.py).

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-# based on https://github.com/pypa/sampleproject/blob/master/setup.py
+# based on https://github.com/pypa/sampleproject/blob/master/setup.py and
+# https://github.com/pysal/pysal/blob/master/setup.py
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
@@ -8,67 +9,73 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+MAJOR = 0
+MINOR = 1
+PATCH = 5
+VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
 
-setup(
-    name='region',
+if __name__ == "__main__":
+    # Get the long description from the README file
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
 
-    version='0.1.4',
+    setup(
+        name='region',
 
-    description='Package offering regionalization algorithms',
-    long_description=long_description,
+        version=VERSION,
 
-    url='https://github.com/pysal/region',
+        description='Package offering regionalization algorithms',
+        long_description=long_description,
 
-    maintainer="PySAL Developers",
-    maintainer_email='pysal-dev@googlegroups.com',
+        url='https://github.com/pysal/region',
 
-    license='BSD 3-clause',
+        maintainer="PySAL Developers",
+        maintainer_email='pysal-dev@googlegroups.com',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        license='BSD 3-clause',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: GIS',
+        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
+            # Indicate who your project is intended for
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: GIS',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: BSD License',
 
-    # What does your project relate to?
-    keywords='regionalization spatial clustering',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'doc', 'test*']),
+        # What does your project relate to?
+        keywords='regionalization spatial clustering',
 
-    # See: https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'geopandas',
-        'libpysal',
-        'networkx<2.0.0',
-        'numpy>=1.10.4',
-        'pulp',
-        'pytest',
-        'scipy',
-        'sklearn',
-    ],
+        # You can just specify the packages manually here if your project is
+        # simple. Or you can use find_packages().
+        packages=find_packages(exclude=['contrib', 'doc', 'test*']),
 
-    python_requires='~=3.3',  # ~=3.3 means >=3.3 and <4
-)
+        # See: https://packaging.python.org/en/latest/requirements.html
+        install_requires=[
+            'geopandas',
+            'libpysal',
+            'networkx<2.0.0',
+            'numpy>=1.10.4',
+            'pulp',
+            'pytest',
+            'scipy',
+            'sklearn',
+        ],
+
+        python_requires='~=3.3',  # ~=3.3 means >=3.3 and <4
+    )
